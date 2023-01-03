f079455ad09ae4d35a73163198d700a7aeb516d5
Nate Gonzales Hess <nhess@uoregon.edu>
2023-01-02 23:07:39 -0800

    simple lerp

--- lerp0.py
+++ lerp0.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import random
 
 height = 512
 width = 512
@@ -18,31 +19,36 @@ def lerp(start, end, dt):
     lerp_list = []
     t = 1
     while t >= 0:
-        x = start[0] + (end[0] - start[0]) * t
-        y = start[1] + (end[1] - start[1]) * t
+        x = int(start[0] + (end[0] - start[0]) * t)
+        y = int(start[1] + (end[1] - start[1]) * t)
         lerp_list.append([x, y])
         t = round(t-dt, 2)
-    return np.int32(lerp_list)
+    return lerp_list
 
 # Lerp for spline
-def lerp_cont(l1, l2):
+def lerp_cont(l1: list, l2: list):
     lerp_list = []
+    fraction = []
     for pt in range(len(l1)):
         t = pt/(len(l1)-1)
-        x = l1[pt][0] + (l2[pt][0] - l1[pt][0]) * t
-        y = l1[pt][1] + (l2[pt][1] - l1[pt][1]) * t
+        x = int(l1[pt][0] + (l2[pt][0] - l1[pt][0]) * t)
+        y = int(l1[pt][1] + (l2[pt][1] - l1[pt][1]) * t)
         lerp_list.append([x, y])
-    return np.int32(lerp_list)
+        fraction.append(round(t, 2))
+    return lerp_list, fraction
 
-
-# Lerp Functions
-dt = .05
+# LERP Functions
+dt = .02
 lp0 = lerp(p0, p1, dt)
 lp1 = lerp(p3, p2, dt)
-spline = lerp_cont(lp0, lp1)
+spline, fraction = lerp_cont(lp0, lp1)
+
 
 # Draw Spline
-canvas = cv2.polylines(canvas, [spline], False, (255, 255, 0), 1, lineType=cv2.LINE_AA)
+for i in range(len(lp0)):
+    canvas = cv2.line(canvas, lp0[i], lp1[i], (32, 32, 0), 1, lineType=cv2.LINE_AA)
+for i in range(len(lp0)):
+    canvas = cv2.circle(canvas, spline[i], 2, (255, 255, 0), -1, lineType=cv2.LINE_AA)
 
 # Draw Handles
 canvas = cv2.circle(canvas, lp0[0], 2, (255, 255, 0), -1, lineType=cv2.LINE_AA)
@@ -51,14 +57,9 @@ canvas = cv2.circle(canvas, lp0[-1], 2, (255, 255, 0), -1, lineType=cv2.LINE_AA)
 canvas = cv2.circle(canvas, lp1[-1], 2, (255, 255, 0), -1, lineType=cv2.LINE_AA)
 canvas = cv2.line(canvas, p0, p1, (255, 255, 255), 1, lineType=cv2.LINE_AA)
 canvas = cv2.line(canvas, p2, p3, (255, 255, 255), 1, lineType=cv2.LINE_AA)
-
-# Output
 #cv2.imwrite(f"{i}.png", canvas)
 cv2.imshow('Spline', canvas)
 cv2.waitKey(32)
-
-
-
 
 
 cv2.imshow('Spline', canvas)
