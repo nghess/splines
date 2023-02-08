57abd8ea6e1d15939d932a08c96a34b60795ecca
Nate Gonzales Hess <nhess@uoregon.edu>
2023-02-08 01:14:47 -0800

    waves exercise

--- trig_exercise.py
+++ trig_exercise.py
@@ -10,45 +10,46 @@ canvas = np.zeros((height, width, 3), dtype=np.uint8)
 # Points for each wave
 loop = 200
 
+
 class MakeSin:
-    def __init__(self, freq=1, steps=loop):
+    def __init__(self, objlist, freq=1, steps=loop):
         self.freq = freq
         self.steps = steps
         # Make sin wave
         self.x = np.linspace(0, 2*np.pi*freq, steps)
         self.y = np.sin(self.x)
+        objlist.append(self)
 
 
 class MakeCos:
-    def __init__(self, freq=1, steps=loop):
+    def __init__(self, objlist, freq=1, steps=loop):
         self.freq = freq
         self.steps = steps
         # Make cosine wave
         self.x = np.linspace(0, 2*np.pi*freq, steps)
         self.y = np.cos(self.x)
+        objlist.append(self)
 
 
 class MakeCombo:
-    def __init__(self, sin, cos):
+    def __init__(self, objlist, sin, cos):
         # Make cosine wave
         self.x = sin.y
         self.y = cos.y
+        objlist.append(self)
 
 
 # Graphics
 class Window:
-    def __init__(self, object, corner=[0, 0], scale=size*.9):
-        self.scale = scale
-        self.corner = corner
-        self.x = object.x
-        self.y = object.y
-
+    def __init__(self, objlist, obj, corner=(0, 0), scale=size*.9):
         # Normalize to square, scale and locate
-        self.x = (self.x-np.min(self.x))/(np.max(self.x)-np.min(self.x))
-        self.y = (self.y-np.min(self.y))/(np.max(self.y)-np.min(self.y))
+        self.x = (obj.x-np.min(obj.x))/(np.max(obj.x)-np.min(obj.x))
+        self.y = (obj.y-np.min(obj.y))/(np.max(obj.y)-np.min(obj.y))
         self.x = np.array([int(x * scale) + corner[0] for x in self.x])
         self.y = np.array([int(y * scale) + corner[1] for y in self.y])
         self.pts = np.column_stack((self.x, self.y))
+        # Add to list
+        objlist.append(self)
 
     def draw(self):
         cv2.polylines(canvas, [self.pts], False, (128, 128, 0), 1, lineType=cv2.LINE_AA)
@@ -60,26 +61,21 @@ change = 0
 sin_waves, cos_waves, cmb_waves = ([], [], [])
 
 for i in range(freqs):
-    sin = MakeSin(freq=change+1)
-    cos = MakeCos(freq=change+1)
-    sin_waves.append(sin)
-    cos_waves.append(cos)
+    sin = MakeSin(sin_waves, freq=change+1)
+    cos = MakeCos(cos_waves, freq=change+1)
     change += 1
 
 # Create Combos
 for i in range(freqs**2):
     for cos in cos_waves:
         for sin in sin_waves:
-            combination = MakeCombo(cos, sin)
-            cmb_waves.append(combination)
+            combination = MakeCombo(cmb_waves, cos, sin)
 
 # Sin and Cos windows
 sin_windows, cos_windows, cmb_windows = ([], [], [])
 for i in range(freqs):
-    sin_plt = Window(sin_waves[i], [0, size*(i+1)])
-    sin_windows.append(sin_plt)
-    cos_plt = Window(cos_waves[i], [size*(i+1), 0])
-    cos_windows.append(cos_plt)
+    sin_plt = Window(sin_windows, sin_waves[i], [0, size*(i+1)])
+    cos_plt = Window(cos_windows, cos_waves[i], [size*(i+1), 0])
 
 # Combo windows
 count, x_loc, y_loc = (0, 0, 0)
@@ -87,8 +83,7 @@ for y in range(freqs):
     y_loc = size * (y+1)
     for x in range(freqs):
         x_loc = size * (x+1)
-        cmb_plt = Window(cmb_waves[count], [x_loc, y_loc])
-        cmb_windows.append(cmb_plt)
+        cmb_plt = Window(cmb_windows, cmb_waves[count], [x_loc, y_loc])
         count += 1
 
 # Draw windows
@@ -98,12 +93,12 @@ for i in range(freqs):
 for i in range(freqs**2):
     cmb_windows[i].draw()
 
+# Set beginning of loop
 frame = 0
-cv2.imwrite("waves.png", canvas)
 
 while True:
-    # Wipe Background
-    background = cv2.imread("waves.png")
+    # Refresh Background
+    background = canvas.copy()
 
     # Draw points
     for cos in cos_windows:
@@ -115,12 +110,12 @@ while True:
 
     # Show Canvas
     cv2.imshow('Spline', background)
-    cv2.imwrite("output/" + str(frame) + "waves.png", background)
+    #cv2.imwrite("output/" + str(frame) + "waves.png", background)
     if cv2.waitKey(16) == ord('q'):
         break
 
     # Looper
     frame += 1
     if frame == loop:
-        #frame = 0
-        break
+        frame = 0
+        #break
